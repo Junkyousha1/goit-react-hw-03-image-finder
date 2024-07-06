3cdd6f6ea7d63c5f928d1dccfcb583e78168676c
Junkyousha1 <fircosmin5828@gmail.com>
2024-07-06 14:16:22 +0300

    finish

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,89 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import React, { Component } from 'react';
+
+import Notiflix, { Notify } from 'notiflix';
+
+import SearchBar from './SearchBar/SearchBar';
+import Gallery from './Gallery/Gallery';
+import Loader from './Loader/Loader';
+import Button from './Button/Button';
+
+export default class App extends Component {
+  state = {
+    URL: 'https://pixabay.com/api/',
+    API_KEY: '43369077-5baf35802a1d034a9e1357834',
+    pictures: [],
+    error: '',
+    status: 'idle',
+    page: 1,
+    query: '',
+    totalHits: null,
+  };
+
+  fetchImg = () => {
+    return fetch(
+      `${this.state.URL}?q=${this.state.query}&page=${this.state.page}&key=${this.state.API_KEY}&image_type=photo&orientation=horizontal&per_page=12`
+    )
+      .then(res => {
+        if (res.ok) {
+          return res.json();
+        }
+        return Promise.reject(new Error('Failed to find any images'));
+      })
+      .then(pictures => {
+        if (!pictures.total) {
+          Notify.error('Did find anything, mate');
+        }
+        const selectedProperties = pictures.hits.map(
+          ({ id, largeImageURL, webformatURL }) => {
+            return { id, largeImageURL, webformatURL };
+          }
+        );
+        this.setState(prevState => {
+          return {
+            pictures: [...prevState.pictures, ...selectedProperties],
+            status: 'resolved',
+            totalHits: pictures.total,
+          };
+        });
+      })
+      .catch(error => this.setState({ error, status: 'rejected' }));
+  };
+
+  componentDidUpdate(prevProps, prevState) {
+    if (this.state.query !== prevState.query) {
+      this.setState({ status: 'pending', pictures: [], page: 1 });
+      this.fetchImg();
+    }
+    if (
+      this.state.query === prevState.query &&
+      this.state.page !== prevState.page
+    ) {
+      this.setState({ status: 'pending' });
+      this.fetchImg();
+    }
+  }
+
+  processSubmit = query => {
+    this.setState({ query });
+  };
+
+  handleLoadMore = () => {
+    this.setState(prevState => {
+      return { page: prevState.page + 1 };
+    });
+  };
+
+  render() {
+    const { pictures, status, totalHits } = this.state;
+    return (
+      <>
+        <SearchBar onSubmit={this.processSubmit} />
+        {pictures.length && <Gallery images={pictures} />}
+        {totalHits > pictures.length && (
+          <Button onClick={this.handleLoadMore} />
+        )}
+        {status === 'pending' && <Loader />}
+      </>
+    );
+  }
+}
